0aba522896bde5bfe186aa23828a15c7ea33f587
Asus <mehrbodmollakazemi81@gmail.com>
2025-10-16 23:32:05 +0330

    RELEASE v0.5.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mehrbodmk.factesimchin"
         minSdk = 28
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.4.1.0"
+        versionCode = 9
+        versionName = "0.5.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
